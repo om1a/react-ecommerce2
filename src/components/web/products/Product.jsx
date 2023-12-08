431e0b7078ea4be2102db9a8f3b1cccbaf793204
unknown <you@example.com>
2023-12-08 13:39:51 -0800

    third commit

--- src/components/web/products/Product.jsx
+++ src/components/web/products/Product.jsx
@@ -2,7 +2,6 @@ import axios from "axios";
 import React, { useContext } from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
-import ReactImageMagnify from 'react-image-magnify';
 import { CartContext } from "../context/Cart.jsx";
 
 
@@ -32,26 +31,14 @@ export default function Product() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-lg-4">
+        <div className="col-lg-6">
             {data.subImages.map((img,index)=>
             <React.Fragment key={index}>
-            <ReactImageMagnify {...{
-                smallImage: {
-                    alt: 'Wristwatch by Ted Baker London',
-                    isFluidWidth: true,
-                    src: img.secure_url
-                },
-                largeImage: {
-                    src: img.secure_url,
-                    width: 1200,
-                    height: 1800
-                },
-                isHintEnabled:true,
-            }} />
+            <img src={img.secure_url} alt="clothes" />
             </React.Fragment>
             )}
         </div>
-        <div className="col-lg-8">
+        <div className="col-lg-6 ps-5">
                 <h2>{data.name}</h2>
                 <p>{data.price}</p>
                 <button className="btn btn-outline-info" onClick={()=>addToCart(data._id)}>Add To Cart</button>
